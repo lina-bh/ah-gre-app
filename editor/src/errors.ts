aa3af447e3844cfcdf0c1855917993e04563df4c
lina <36717206+lina-bh@users.noreply.github.com>
2023-07-29 19:05:20 +0100

    move

--- editor/src/errors.ts
+++
@@ -1,8 +0,0 @@
-export class NullTtError extends Error {
-  constructor(
-    message: string = "cannot access timetable when none was selected"
-  ) {
-    super(message)
-    this.name = "NullTtError"
-  }
-}
